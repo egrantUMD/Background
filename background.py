be5f5cfafc266edc4a636fa9d19e876d9cc76e3f
egrantUMD <91499830+egrantUMD@users.noreply.github.com>
2021-10-19 18:27:28 +0000

    stuff

--- background.py
+++ background.py
@@ -7,13 +7,13 @@ import json
 while True:
   with open('led-pwm.txt', 'r') as f:
     data = json.load(f)
-    pin = int(data['pin'])
+    p1 = int(data['PIN'])
     slide = int(data['slider1'])
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
+    GPIO.setup(p1, GPIO.OUT)
 
-    pwm = GPIO.PWM(pin, 100)
+    pwm = GPIO.PWM(p1, 100)
     pwm.ChangeDutyCycle(slide)
     time.sleep(0.2)
 
